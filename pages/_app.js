26cb3bc0ba3a8617560fc794d7263236b6ba51d8
artmucha <arti.mucha@gmail.com>
2023-04-20 17:38:35 +0200

    add mui styles configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,23 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import * as React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import theme from '../styles/theme';
+import createEmotionCache from '../styles/createEmotionCache';
+
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Gym tracker</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
