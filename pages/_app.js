e434879aa87c8a3c8a88d4297495ee8ae8b3a4df
artmucha <arti.mucha@gmail.com>
2023-04-25 17:31:10 +0200

    modified app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,26 @@
 import * as React from 'react';
 import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
 import { CacheProvider } from '@emotion/react';
-import theme from '../styles/theme';
-import createEmotionCache from '../styles/createEmotionCache';
+
+import { AuthProvider } from '@/providers/AuthProvider';
+import ThemeProvider from '@/styles/theme';
+import createEmotionCache from '@/styles/createEmotionCache';
 
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>Gym tracker</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <AuthProvider>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>Gym tracker</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </AuthProvider>
   );
 }
